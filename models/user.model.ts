712546ad7c3c8c92672a570c2670c7a70e03cde3
Amelie Barre <amliebarre@gmail.com>
2026-07-13 16:22:18 +0200

    implementing the user schema using Drizzle ORM

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,5 +1,12 @@
-import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
   id: uuid().primaryKey().defaultRandom(),
+  firstname: varchar('first_name', { length: 55 }).notNull(),
+  lastname: varchar('last_name', { length: 55 }).notNull(),
+  email: varchar({ length: 255 }).notNull(),
+  password: text().notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
 });
