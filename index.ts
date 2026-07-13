2bac24d42ac080c1aee165b79d244d275fd9b742
Amelie Barre <amliebarre@gmail.com>
2026-07-13 15:58:20 +0200

    set up Express

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT ?? 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({ status: 'Server is up and running' });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
