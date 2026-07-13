540ba20a86b466c8d572c40b02e4205de22adc37
Amelie Barre <amliebarre@gmail.com>
2026-07-13 23:52:10 +0200

    building a login route with JWT authentication and token response

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
 import { z } from 'zod';
-import { db } from '../db';
+import jwt from 'jsonwebtoken';
 import { usersTable } from '../models';
 import { hashPasswordWithSalt } from '../utils/hash';
 import { getUserByEmail, insertUser } from '../services/user.service';
-import { signupPostRequestBodySchema } from '../validation/request.validation';
+import {
+  signupPostRequestBodySchema,
+  loginPostRequestBodySchema,
+} from '../validation/request.validation';
 
 const router = express.Router();
 
@@ -12,6 +15,8 @@ type SignupBody = Pick<
   typeof usersTable.$inferInsert,
   'firstname' | 'lastname' | 'email' | 'password'
 >;
+
+type LoginBody = Pick<typeof usersTable.$inferInsert, 'email' | 'password'>;
 
 router.post(
   '/signup',
@@ -46,6 +51,50 @@ router.post(
     );
 
     return res.status(201).json({ data: { userId: user.id } });
+  },
+);
+
+router.post(
+  '/login',
+  async (req: Request<{}, {}, LoginBody>, res: Response) => {
+    const validationResult = await loginPostRequestBodySchema.safeParseAsync(
+      req.body,
+    );
+
+    if (validationResult.error) {
+      return res
+        .status(400)
+        .json({ error: z.flattenError(validationResult.error) });
+    }
+
+    const { email, password } = validationResult.data;
+
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ error: `User with email ${email} does not exist.` });
+    }
+
+    const { password: hashedPassword } = hashPasswordWithSalt(
+      password,
+      user.salt,
+    );
+
+    if (user.password !== hashedPassword) {
+      return res.status(400).json({ error: `Invalid password.` });
+    }
+
+    const jwtSecret = process.env.JWT_SECRET;
+
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+
+    const token = jwt.sign({ id: user.id }, jwtSecret);
+
+    return res.json({ token });
   },
 );
 
