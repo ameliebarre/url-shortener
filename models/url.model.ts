c7297b8f26b55749257886a1d55ac45b3f39f5a3
Amelie Barre <amliebarre@gmail.com>
2026-07-15 22:46:52 +0200

    creating schema for shortened url with Drizzle

---
+++ models/url.model.ts
@@ -0,0 +1,14 @@
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { usersTable } from './user.model';
+
+export const urlsTable = pgTable('url', {
+  id: uuid().primaryKey().defaultRandom(),
+  shortcode: varchar('code', { length: 155 }).notNull(),
+  target: varchar('target_url').notNull(),
+  userId: uuid('user_id')
+    .references(() => usersTable.id)
+    .notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+});
