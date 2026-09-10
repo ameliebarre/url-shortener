448b950332128a91bdf41e6a134365a995251f43
Amelie Barre <amliebarre@gmail.com>
2026-09-10 14:55:14 +0200

    adding index on url.user_id

--- apps/api/models/url.model.ts
+++ apps/api/models/url.model.ts
@@ -1,16 +1,20 @@
-import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { index, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 import { usersTable } from '@/models';
 
-export const urlsTable = pgTable('url', {
-  id: uuid().primaryKey().defaultRandom(),
-  shortcode: varchar('code', { length: 155 }).notNull().unique(),
-  targetUrl: varchar('target_url').notNull(),
-  userId: uuid('user_id')
-    .references(() => usersTable.id)
-    .notNull(),
+export const urlsTable = pgTable(
+  'url',
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    shortcode: varchar('code', { length: 155 }).notNull().unique(),
+    targetUrl: varchar('target_url').notNull(),
+    userId: uuid('user_id')
+      .references(() => usersTable.id)
+      .notNull(),
 
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
-  expiresAt: timestamp('expires_at'),
-});
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+    expiresAt: timestamp('expires_at'),
+  },
+  (table) => [index('url_user_id_idx').on(table.userId)],
+);
