86d84edeae630df64367fc5b1bf0daa847fe656a
Amelie Barre <amliebarre@gmail.com>
2026-09-11 14:04:57 +0200

    [front] adding homepage banner hero

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../components/Header';
+import { HomeHero } from '../components/HomeHero';
 
 export function HomePage() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
+      <HomeHero />
     </div>
   );
 }
